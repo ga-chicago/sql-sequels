a7848c79a305e24f54b668c7f2fad0ee61d34f72
code-for-coffee <jtraverjr@gmail.com>
2016-06-22 11:10:47 -0500

    project started

---
+++ Sequels/app/src/main/java/org/codeforcoffee/sequels/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package org.codeforcoffee.sequels;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by codeforcoffee on 6/22/16.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DB_VERSION = 1; // change as we upgrade
+    public static final String DB_NAME = "ReallyBadSequels.db"; // name of your .db
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
